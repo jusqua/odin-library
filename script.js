4e22f04134fff40709ccabcfd584bf76d0dca4de
Ádrian Gama <adrian.gama.se@gmail.com>
2022-12-01 12:20:08 -0300

    feat: nord pallete, new header and footer behavior

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
-const bookShelf = document.getElementById('book-shelf');
-const addBookButton = document.getElementById('add-book');
-const header = document.querySelector('header');
-const footer = document.querySelector('footer');
+window.addEventListener('DOMContentLoaded', () => {
+  const bookShelf = document.getElementById('book-shelf');
+  const addBookButton = document.getElementById('add-book');
+  const header = document.querySelector('header');
+  const footer = document.querySelector('footer');
 
-function handleScroll() {
-  if (window.scrollY !== 0 && !header.classList.contains('elevation')) {
-    header.classList.add('elevation');
-  } else if (window.scrollY === 0) {
-    header.classList.remove('elevation');
+  function handleHeaderFooterBehavior() {
+    if (window.scrollY !== 0 && !header.classList.contains('elevation')) {
+      header.classList.add('elevation');
+    } else if (window.scrollY === 0) {
+      header.classList.remove('elevation');
+    }
+    if (window.scrollY !== document.body.scrollHeight - window.innerHeight && !footer.classList.contains('elevation')) {
+      footer.classList.add('hide');
+    } else if (window.scrollY === document.body.scrollHeight - window.innerHeight) {
+      footer.classList.remove('hide');
+    }
   }
-  if (window.scrollY !== document.body.scrollHeight - window.innerHeight && !footer.classList.contains('elevation')) {
-    footer.classList.add('elevation');
-  } else if (window.scrollY === document.body.scrollHeight - window.innerHeight) {
-    footer.classList.remove('elevation');
+
+  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.documentElement.classList.add('dark');
   }
-}
 
-if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  document.documentElement.classList.add('dark');
-}
+  window.addEventListener('scroll', handleHeaderFooterBehavior);
+  window.addEventListener('resize', handleHeaderFooterBehavior);
+  (new ResizeObserver(handleHeaderFooterBehavior)).observe(document.body);
 
-window.addEventListener('scroll', handleScroll);
-
-addBookButton.addEventListener('click', () => {
-  bookShelf.innerHTML += '<div class="book elevation"></div>';
-  handleScroll();
+  addBookButton.addEventListener('click', () => {
+    bookShelf.innerHTML += '<div class="book elevation"></div>';
+  });
 });
 
+window.addEventListener('load', () => document.body.classList.remove('preload'));
