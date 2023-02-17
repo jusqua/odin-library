0cd8cd4fdfecb1303d04bfc6e87102d3c8349133
Ádrian Gama <adrian.gama.se@gmail.com>
2023-02-17 17:24:12 -0300

    (feat) change theme button

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-window.addEventListener('DOMContentLoaded', () => {
-  const dark = localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches);
-  document.documentElement.classList[dark ? 'add' : 'remove']('dark');
+function toogleTheme() {
+  const button = document.getElementById('change-theme');
+  if (document.documentElement.classList.contains('dark'))
+    button.classList.replace('ph-sun-bold', 'ph-moon-bold');
+  else
+    button.classList.replace('ph-moon-bold', 'ph-sun-bold');
+  document.documentElement.classList.toggle('dark');
+}
 
+window.addEventListener('DOMContentLoaded', () => {
   const bookshelf = document.getElementById('bookshelf');
+
 
   for (let i = 0; i < 9; i++) {
     const book = document.createElement('button');
     book.className = 'bg-zinc-400 dark:bg-zinc-700 drop-shadow hover:drop-shadow-2xl h-48 rounded-xl transition-[drop-shadow] duration-350';
     bookshelf.appendChild(book);
   }
+
+  document.getElementById('change-theme').addEventListener('click', toogleTheme);
+
+  if (window.matchMedia('(prefers-color-scheme: dark)').matches)
+    toogleTheme();
 });
